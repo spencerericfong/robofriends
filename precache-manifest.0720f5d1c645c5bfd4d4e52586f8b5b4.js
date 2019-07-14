19c330cdf549c0361163d2eeb13ee85184953cfb
Spencer Fong <34435767+fongse99@users.noreply.github.com>
2019-07-13 18:13:14 -0700

    Updates

---
+++ precache-manifest.0720f5d1c645c5bfd4d4e52586f8b5b4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "063861f399e5c4a94a2a711b80eea167",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "165c83f85a4584eb65d7",
+    "url": "/robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "f35636360733fd353938",
+    "url": "/robofriends/static/css/main.eca95048.chunk.css"
+  },
+  {
+    "revision": "165c83f85a4584eb65d7",
+    "url": "/robofriends/static/js/2.590933e2.chunk.js"
+  },
+  {
+    "revision": "f35636360733fd353938",
+    "url": "/robofriends/static/js/main.7967e0c2.chunk.js"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
